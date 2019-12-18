020e845d63c25144ebbdcc23cfd9eed05c02215d
pedrosc97 <pedro.sc.97@gmail.com>
2019-12-17 18:26:11 -0600

    Rewrite of boat and station xbee classes

--- scripts/boat_transceiver.py
+++ scripts/boat_transceiver.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
+import argparse
 import rospy
+import time
+import sys
 from std_msgs.msg import String
-from std_msgs.msg import Empty
 from digi.xbee.devices import XBeeDevice
+
+parser = argparse.ArgumentParser()
+parser.add_argument('dev')
+args = parser.parse_args()
 
 #****************************************************************************************#
 # Replace with the serial port where your local module is connected to.
-PORT = "/dev/ttyUSB0"
+PORT = args.dev
 # Replace with the baud rate of your local module.
 BAUD_RATE = 9600
 # El nodo XBee con el que se quiere comunicar.
@@ -17,10 +23,28 @@ ROS_RATE = 100
 #****************************************************************************************#
 
 class XbeeBoat:
-    def __init__(self):
+    def __init__(self, _port, _baud_rate, _remote_id, _ros_rate):
         
-        self._mon_sub = rospy.Subscriber("data_input", String, self.data_callback)
+        # Initialize and configure the DigiXTend Xbee Device
+        self.device = XBeeDevice(_port, _baud_rate)
+
+        self.device.open()    
+        if not self.device.is_open():
+            print('[USV] Device could not be opened.')
+            raise Exception()
         
+        self.device.flush_queues()
+        self.xnetwork = self.device.get_network()
+        self.remote_device = self.xnetwork.discover_device(_remote_id)
+        if self.remote_device is None:
+            print('[USV] Could not find the remote device.')
+            self.device.close()
+            raise Exception()
+            
+        print('[USV] Digi XTend device initialized.')
+
+        # ROS Configuration
+        self._mon_sub = rospy.Subscriber("/usv_comms/boat_transceiver/data_input", String, self.data_callback)
         self.course_pub = rospy.Publisher("/usv_comms/boat_transceiver/course_config", String, queue_size=10)
         self.start_pub = rospy.Publisher("/usv_comms/boat_transceiver/start_mission", Empty, queue_size=10)
         self.stop_pub = rospy.Publisher("/usv_comms/boat_transceiver/stop_mission", Empty, queue_size=10)
@@ -28,67 +52,50 @@ class XbeeBoat:
         self.empty_msg = Empty()
         self.boat_data = String()
 
+        self.comm_active = True
+        print('[USV] Awaiting conversation...\n')
+
     def data_callback(self, _data):
-        self.boat_data = _data
-        print('Sending data: ', self.boat_data)
-        # Send the monitoring data to station
-        device.send_data_async(remote_device, xbee_node.boat_data)
-        #rospy.logwarn(self.powerL)
+        self.boat_data = _data.data
+        print('[USV] Sending data: ', self.boat_data)
+        self.device.send_data_async(self.remote_device, self.boat_data)
 
 def main():
-    rospy.init_node('boat_transceiver', anonymous=True)
-    rate = rospy.Rate(ROS_RATE) # 100hz
-    
     print(" +-------------------------------------------------+")
     print(" |                       Boat                      |")
     print(" +-------------------------------------------------+\n")
     
-    device = XBeeDevice(PORT, BAUD_RATE)
-    xbee_node = XbeeBoat()
+    rospy.init_node('boat_transceiver', anonymous=True)
 
     try:
-        device.open()
-        device.flush_queues()
-        xbee_network = device.get_network()
-        remote_device = xbee_network.discover_device(REMOTE_NODE_ID)
-        if remote_device is None:
-            print("Could not find the remote device")
-            exit(1)
-        
-        print("Waiting conversation...\n")
-        
-        #Variable to stop the conversation 
-        commActive = True
+        usv = XbeeStation(PORT, BAUD_RATE, REMOTE_NODE_ID, ROS_RATE)
+    except:
+        print('[USV] Digi XTend device could not be initialized.')
+        sys.exit(1)
 
+    try:
         while not rospy.is_shutdown() and commActive:
-
             #Read data and chek if something has been received 
-            xbee_message = device.read_data()
+            xbee_message = usv.device.read_data()
+            
             if xbee_message is not None:
-                
                 #Decode and print the message 
                 message = xbee_message.data.decode()
-                print("Received Message: " , message)
-
                 if message[0] == 'c':
-                    xbee_node.course_pub.publish(message[1])
-                    device.send_data_async(remote_device, xbee_node.boat_data)
+                    usv.course_pub.publish(message[1])
+                    usv.device.send_data_async(usv.remote_device, 'Changing to course ' + message[1])
                 elif message == 's':
-                    xbee_node.start_mission.publish(xbee_node.empty_msg)
-                    device.send_data_async(remote_device, 'Starting mission...')
+                    usv.start_pub.publish(usv.empty_msg)
+                    usv.device.send_data_async(usv.remote_device, 'Starting mission...')
                 elif message == 'k':
-                    xbee_node.stop_mission.publish(xbee_node.empty_msg)
-                    device.send_data_async(remote_device, 'Stopping mission.')
-                elif message == 'exit':
-                    commActive = False
-            
-            #rospy.spinOnce()
-            rate.sleep()
-
+                    usv.stop_pub.publish(usv.empty_msg)
+                    usv.device.send_data_async(usv.remote_device, 'Stopping mission.')
+            usv.ros_rate.sleep()
     #If the device is not closed, close it.
     finally:
-        if device is not None and device.is_open():
-            device.close()
+        print('[USV] Terminating Session...')
+        if usv.device is not None and usv.device.is_open():
+            usv.device.close()
 
 if __name__ == "__main__":
     try:
--- scripts/station_transceiver.py
+++ scripts/station_transceiver.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
+import argparse
 import rospy
 import time
+import sys
 from std_msgs.msg import String
 from digi.xbee.devices import XBeeDevice
 
+parser = argparse.ArgumentParser()
+parser.add_argument('dev')
+args = parser.parse_args()
+
 #****************************************************************************************#
 # Replace with the serial port where your local module is connected to.
-PORT = "/dev/ttyUSB0" #La estacion
+PORT = args.dev
 # Replace with the baud rate of your local module.
 BAUD_RATE = 9600
 # El nodo XBee con el que se quiere comunicar.
@@ -17,55 +23,67 @@ ROS_RATE = 100
 #****************************************************************************************#
 
 class XbeeStation:
-    def __init__(self):
+    def __init__(self, _port, _baud_rate, _remote_id, _ros_rate):
+        
+        # Initialize and configure the DigiXTend Xbee Device
+        self.device = XBeeDevice(_port, _baud_rate)
+    
+        self.device.open()    
+        if not self.device.is_open():
+            print('[STATION] Device could not be opened.')
+            raise Exception()
+
+        self.device.flush_queues()
+        self.xnetwork = self.device.get_network()
+        self.remote_device = self.xnetwork.discover_device(_remote_id)
+        if self.remote_device is None:
+            print('[STATION] Could not find the remote device.')
+            self.device.close()
+            raise Exception()
+            
+        print('[STATION] Digi XTend device initialized.')
+
+        # ROS Configuration
+        self.ros_rate = rospy.Rate(_ros_rate)
         self.boat_data_pub = rospy.Publisher('/usv_comms/station_transceiver/boat_data', String, queue_size=10)
+        self._config_sub = rospy.Subscriber('/usv_comms/station_transceiver/course_config', String, self.config_callback)
+        print('[STATION] ROS Node initialized.')
+
+        self.comm_active = True
+        print('[STATION] Awaiting conversation...\n')
+
+    def config_callback(self, _config):
+        print('[STATION] Sending following message: ' + str(_config.data))
+        self.device.send_data_async(self.remote_device, str(_config.data))
+        if str(_config.data) == 'exit':
+            self.comm_active = False
 
 def main():
-    rospy.init_node('station_transceiver', anonymous=True)
-    rate = rospy.Rate(ROS_RATE) # 100hz
-    
     print(" +--------------------------------------+")
     print(" |                Station               |")
     print(" +--------------------------------------+\n")
 
-    device = XBeeDevice(PORT, BAUD_RATE)
-    xbee_node = XbeeStation()
+    rospy.init_node('station_transceiver', anonymous=True)
     
     try:
-        device.open()
-        xbee_network = device.get_network()
-        remote_device = xbee_network.discover_device(REMOTE_NODE_ID)
-        if remote_device is None:
-            print("Could not find the remote device")
-            exit(1)
+        station = XbeeStation(PORT, BAUD_RATE, REMOTE_NODE_ID, ROS_RATE)
+    except:
+        print('[STATION] Digi XTend device could not be initialized.')
+        sys.exit(1)
 
-        print("Waiting conversation...\n")
-        
-        #Variable to stop the conversation 
-        commActive = True
-
-        while not rospy.is_shutdown() and commActive:
-            action = input("Command: ")
-            device.send_data_async(remote_device, action)
-            time.sleep(1)
-            xbee_message = device.read_data()
-
+    try:
+        while not rospy.is_shutdown() and station.comm_active:
+            
+            xbee_message = station.device.read_data()
             if xbee_message is not None:
-                #Print the message and 
                 message = xbee_message.data.decode()
-                print("Received Message: " , message)
-                xbee_node.boat_data_pub.publish(str(message))
+                station.boat_data_pub.publish(str(message))
 
-            #print("Success")
-            if action == "exit":
-                commActive = False
-
-            #rospy.spinOnce()
-            rate.sleep()
-
+            station.ros_rate.sleep()
     finally:
-        if device is not None and device.is_open():
-            device.close()
+        print('[STATION] Terminating Session...')
+        if station.device is not None and station.device.is_open():
+            station.device.close()
 
 if __name__ == "__main__":
     try:
